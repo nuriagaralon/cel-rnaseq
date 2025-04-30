7d2da3e4844268c76594896936254811d25974b5
Nuria Garriga Alonso <60143046+nuriagaralon@users.noreply.github.com>
2025-04-30 13:07:27 +0200

    benchmark plot update

--- workflow/scripts/benchmarks.R
+++ workflow/scripts/benchmarks.R
@@ -1,8 +1,22 @@
-library(ggplot2)
+library(tidyverse)
 library(ggrepel)
 
-setwd()
+benchmarks <- read.delim(file = "workflow/stats/benchmarks_aggregated.tsv", sep = "\t", header = TRUE)
 
-benchmarks <- read.delim(file = "20250314-131457_benchmarks.tsv", sep = "\t", header = TRUE)
+benchmarks <- benchmarks |> drop_na() |> filter(cpu_time>1) |> group_by(rule) |> summarise(cpu_time_mean = mean(cpu_time), cpu_time_stdev = sd(cpu_time), max_rss_mean = mean(max_rss), max_rss_stdev = sd(max_rss))
 
-plot <- ggplot(benchmarks, aes(cpu_time, max_rss, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
+bench_data <- benchmarks |> filter(str_detect(rule, "qc"))
+
+plot_qc <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
+
+bench_data <- benchmarks |> filter(str_detect(rule, "trim/"))
+
+plot_trim <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
+
+bench_data <- benchmarks |> filter(str_detect(rule, "hisat|star"))
+
+plot_align <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
+
+bench_data <- benchmarks |> filter(str_detect(rule, "expression|salmon|stringtie"))
+
+plot_expr <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
