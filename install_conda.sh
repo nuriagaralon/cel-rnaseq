62808159a4ff532f4de80c1490d158f1efec367e
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2026-05-01 18:48:28 +0200

    Update env config scripts

--- install_conda.sh
+++ install_conda.sh
@@ -5,7 +5,7 @@ wget https://github.com/conda-forge/miniforge/releases/download/24.11.3-0/Minifo
 # wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 
 bash Miniforge3-Linux-x86_64.sh
-source ~/.bashrc
+exec bash
 
 # CREATE ENVIRONMENT
 conda env create -f config/snake_env.yaml
