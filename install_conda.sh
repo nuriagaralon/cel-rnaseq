db7ce273222aa1e4d68395236a4a48d067ae6a68
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2026-05-01 18:09:44 +0200

    Update install script

--- install_conda.sh
+++ install_conda.sh
@@ -7,9 +7,5 @@ wget https://github.com/conda-forge/miniforge/releases/download/24.11.3-0/Minifo
 bash Miniforge3-Linux-x86_64.sh
 source ~/.bashrc
 
-# CLONE PIPELINE REPOSITORY
-git clone https://github.com/nuriagaralon/cel-rnaseq.git
-cd cel-rnaseq
-
 # CREATE ENVIRONMENT
 conda env create -f config/snake_env.yaml
