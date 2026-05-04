a48b660f5a6af54ed200d2efd49baf33c00e7a7c
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2026-05-05 01:26:19 +0200

    Update benchmark plot script

--- workflow/scripts/benchmarks.R
+++ workflow/scripts/benchmarks.R
@@ -1,22 +1,37 @@
 library(tidyverse)
 library(ggrepel)
 
+# Get data
 benchmarks <- read.delim(file = "workflow/stats/benchmarks_aggregated.tsv", sep = "\t", header = TRUE)
 
-benchmarks <- benchmarks |> drop_na() |> filter(cpu_time>1) |> group_by(rule) |> summarise(cpu_time_mean = mean(cpu_time), cpu_time_stdev = sd(cpu_time), max_rss_mean = mean(max_rss), max_rss_stdev = sd(max_rss))
+benchmarks <- benchmarks |> drop_na() |> filter(cpu_time>1) |> group_by(rule) |> 
+  summarise(cpu_time_mean = mean(cpu_time), 
+            cpu_time_stdev = sd(cpu_time), 
+            max_rss_mean = mean(max_rss), 
+            max_rss_stdev = sd(max_rss),
+            .groups = "drop") |>
+  mutate(
+    category = case_when(
+      str_detect(rule, "qc") ~ "QC",
+      str_detect(rule, "trim/") ~ "Trimming",
+      str_detect(rule, "hisat|star") ~ "Alignment",
+      str_detect(rule, "expression|salmon|stringtie") ~ "Expression"
+    )
+  )
 
-bench_data <- benchmarks |> filter(str_detect(rule, "qc"))
+benchmarks$category <- factor(
+  benchmarks$category,
+  levels = c("QC", "Expression", "Alignment", "Trimming")
+)
 
-plot_qc <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
-
-bench_data <- benchmarks |> filter(str_detect(rule, "trim/"))
-
-plot_trim <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
-
-bench_data <- benchmarks |> filter(str_detect(rule, "hisat|star"))
-
-plot_align <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
-
-bench_data <- benchmarks |> filter(str_detect(rule, "expression|salmon|stringtie"))
-
-plot_expr <- ggplot(bench_data, aes(cpu_time_mean, max_rss_mean, label = rule)) + geom_point() + geom_text_repel() + labs(x="CPU time (s)", y="Memory usage (MB)")
+# Plot
+ggplot(benchmarks, aes(cpu_time_mean, max_rss_mean, label = rule)) +
+    geom_point(size = 2) +
+    geom_errorbar(aes(xmin = cpu_time_mean - cpu_time_stdev,
+                        xmax = cpu_time_mean + cpu_time_stdev), orientation = "y") +
+    geom_errorbar(aes(ymin = max_rss_mean - max_rss_stdev,
+                        ymax = max_rss_mean + max_rss_stdev)) +
+    geom_text_repel(size = 3, max.overlaps = 50) +
+    labs(x="CPU time (s)", y="Peak RSS (MB)") +
+    facet_wrap(~ category, scales = "free") +
+    theme_bw()
