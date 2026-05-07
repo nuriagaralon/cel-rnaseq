c5d0a6f02bf99f7f4448f445be57eee4211cc39e
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2026-05-07 13:16:18 +0200

    Clean scripts

--- workflow/scripts/benchmarks.R
+++ workflow/scripts/benchmarks.R
@@ -1,13 +1,18 @@
+# Generate benchmark plots from aggregated benchmark files
 library(tidyverse)
 library(ggrepel)
 
-# Get data
-benchmarks <- read.delim(file = "workflow/stats/benchmarks_aggregated.tsv", sep = "\t", header = TRUE)
+# Get data (Can be changed to benchmarks_aggregated_B)
+benchmarks <- read.delim(file = "project_outputs/benchmarks_aggregated_A.tsv",
+                         sep = "\t", header = TRUE)
 
-benchmarks <- benchmarks |> drop_na() |> filter(cpu_time>1) |> group_by(rule) |> 
-  summarise(cpu_time_mean = mean(cpu_time), 
-            cpu_time_stdev = sd(cpu_time), 
-            max_rss_mean = mean(max_rss), 
+benchmarks <- benchmarks |>
+  drop_na() |>
+  filter(cpu_time > 1) |>
+  group_by(rule) |>
+  summarise(cpu_time_mean = mean(cpu_time),
+            cpu_time_stdev = sd(cpu_time),
+            max_rss_mean = mean(max_rss),
             max_rss_stdev = sd(max_rss),
             .groups = "drop") |>
   mutate(
@@ -15,23 +20,24 @@ benchmarks <- benchmarks |> drop_na() |> filter(cpu_time>1) |> group_by(rule) |>
       str_detect(rule, "qc") ~ "QC",
       str_detect(rule, "trim/") ~ "Trimming",
       str_detect(rule, "hisat|star") ~ "Alignment",
-      str_detect(rule, "expression|salmon|stringtie") ~ "Expression"
+      str_detect(rule, "expression|salmon|stringtie|rsem") ~ "Expression"
     )
   )
 
+# Sort facets
 benchmarks$category <- factor(
   benchmarks$category,
-  levels = c("QC", "Expression", "Alignment", "Trimming")
+  levels = c("QC", "Trimming", "Alignment", "Expression")
 )
 
 # Plot
 ggplot(benchmarks, aes(cpu_time_mean, max_rss_mean, label = rule)) +
-    geom_point(size = 2) +
-    geom_errorbar(aes(xmin = cpu_time_mean - cpu_time_stdev,
-                        xmax = cpu_time_mean + cpu_time_stdev), orientation = "y") +
-    geom_errorbar(aes(ymin = max_rss_mean - max_rss_stdev,
-                        ymax = max_rss_mean + max_rss_stdev)) +
-    geom_text_repel(size = 3, max.overlaps = 50) +
-    labs(x="CPU time (s)", y="Peak RSS (MB)") +
-    facet_wrap(~ category, scales = "free") +
-    theme_bw()
+  geom_point(size = 2) +
+  geom_errorbar(aes(xmin = cpu_time_mean - cpu_time_stdev,
+                    xmax = cpu_time_mean + cpu_time_stdev), orientation = "y") +
+  geom_errorbar(aes(ymin = max_rss_mean - max_rss_stdev,
+                    ymax = max_rss_mean + max_rss_stdev)) +
+  geom_text_repel(size = 3, max.overlaps = 50, box.padding = 0.5) +
+  labs(x = "CPU time (s)", y = "Peak RSS (MB)") +
+  facet_wrap(~ category, scales = "free") +
+  theme_bw()
--- workflow/scripts/compare_stringtie_featurecounts.R
+++ workflow/scripts/compare_stringtie_featurecounts.R
@@ -1,30 +1,32 @@
+# Compares quantified genes/correlation of gene counts
+# for StringTie and featureCounts (samples Exp_2_3, Neg_1_2)
+
 library(tidyverse)
 
 # Load files
-fc1 <- read_tsv("Exp_2_3_gene_counts.tsv", comment = "#") |>
+fc1 <- read_tsv("project_outputs/Exp_2_3_gene_counts.tsv", comment = "#") |>
   select("Geneid", starts_with("results")) |>
   rename("FC_Exp_2_3" = starts_with("results"))
 
-fc2 <- read_tsv("Neg_1_2_gene_counts.tsv", comment = "#") |>
+fc2 <- read_tsv("project_outputs/Neg_1_2_gene_counts.tsv", comment = "#") |>
   select("Geneid", starts_with("results")) |>
   rename("FC_Neg_1_2" = starts_with("results"))
 
-st <- read_csv("gene_count_matrix.csv")
+st <- read_csv("project_outputs/st_gene_count_matrix.csv")
 
-# structure featurecounts
+# Structure featureCounts
 fc <- full_join(fc1, fc2, by = "Geneid")
 
 # Fix gene ID stringtie
-st <- st |> mutate(Geneid = sub("\\|.*$", "", gene_id)) |>
-  rename("ST_Neg_1_2" = Neg_1_2, "ST_Exp_2_3" = Exp_2_3 ) |>
+st <- st |>
+  mutate(Geneid = sub("\\|.*$", "", gene_id)) |>
+  rename("ST_Neg_1_2" = Neg_1_2, "ST_Exp_2_3" = Exp_2_3) |>
   select(-gene_id)
 
-# intersect 
+# Common genes, genes unique to featureCounts and to StringTie
 common <- inner_join(st, fc, by = "Geneid")
-
 only_fc <- anti_join(fc, st, by = "Geneid")
 only_st <- anti_join(st, fc, by = "Geneid")
-
 
 # Pearson correlation for sample Exp_2_3
 cor.test(
