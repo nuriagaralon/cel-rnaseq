cc8428d2f59da1ed67cf051d35b05e69e80a9bab
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2026-05-30 09:09:25 +0200

    DGE scripts and renv

---
+++ workflow/scripts/dge_aggregate.R
@@ -0,0 +1,76 @@
+# Aggregate expression data
+
+#--------------------------
+# Author: Núria Garriga Alonso
+# GitHub: @nuriagaralon
+# Repository: https://github.com/nuriagaralon/cel-rnaseq
+#--------------------------
+
+# Libraries
+library(tidyverse)
+
+# Feature table mapping
+#features_df <- read_tsv(snakemake@input[['features']])
+features_df <- read_tsv("raw_data/references/GCF_000002985.6_WBcel235_feature_table.txt")
+
+features_df <- features_df |>
+  select(symbol, GeneID, locus_tag) |>
+  distinct() |>
+  mutate(across(everything(), as.character))
+
+
+# Build counts dataframe
+files <- list.files(
+  "results/expression",
+  pattern = "_gene_counts.tsv$",
+  full.names = TRUE
+)
+
+
+count_tables <- files |>
+  map(function(file){
+    df <- read_tsv(file, comment = "#") |>
+            select(Geneid, starts_with("results"))
+    
+    names(df)[2] <- names(df)[2] |>
+      basename() |>
+      str_remove("_Aligned.sortedByCoord.out.bam")
+    
+    df
+  })
+
+count_df <- purrr::reduce(count_tables, full_join, by = "Geneid")
+count_df <- column_to_rownames(count_df, "Geneid")
+
+# Build metadata dataframe
+#meta_df <- read_tsv(snakemake@input[['metadata']])
+meta_df <- read_tsv("raw_data/samples/SampleMetadata.tsv")
+
+# Sample list
+count_samples <- names(count_df)
+metadata_samples <- meta_df$Sample.ID
+
+# SAFETY CHECK: SAMPLES AND NAMES
+if(!setequal(count_samples, metadata_samples)){
+  stop(
+    paste0(
+      "Samples do not match between counts matrix and metadata.\n",
+      "Missing in metadata: ",
+      paste(setdiff(count_samples, metadata_samples), collapse = ", "), "\n",
+      "Missing in counts: ",
+      paste(setdiff(metadata_samples, count_samples), collapse = ", ")      
+    )
+  )
+}
+
+# Reorder metadata
+meta_df <- meta_df[match(count_samples, metadata_samples), ]
+
+# SAFETY CHECK: SAMPLE ORDER
+if(!identical(count_samples, meta_df$Sample.ID)){
+  stop("Metadata sample ordering failed after matching")
+}
+
+# Save objects
+save(list = c("count_df", "meta_df", "features_df"), file = "results/dge/data.RData")
+
